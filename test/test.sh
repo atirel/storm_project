770db69a34006b8f4571e5caf765595c4fa5b28b
guest <25740490+atirel@users.noreply.github.com>
2018-06-26 10:16:26 +0200

    added a better indent and a better place to run the tests, added also a percentage of success and some verbosity

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-cd src-test-files
-
+place=`pwd`/src-test-files
+cd $place
 function binGenerator {
    clang -emit-llvm -c -o $1.bc $1.c
    opt -load ~/storm/llvm-joujou/test/storm_project/build/DoubleStore/LLVMDoubleStore.so -DoubleStore < $1.bc > $2.bc 
@@ -18,7 +17,10 @@ function binChecker {
    fi
    echo 0
 }
-
+echo "Do not worry if you see useless stuff for testing purposes, the execution is written over cerrs and as far as my knowledge goes, i don't know a way to hide it while saving it"
+echo "for convention, everything will be erased before showing test results"
+echo "enter a key to continue"
+read continuation
 
 let "i=1"
 for file in $(ls)
@@ -31,21 +33,41 @@ do
    let "i+=1"
 done
 
-
+clear
+let "i=1"
+let "j=0"
 for file in $(ls)
 do
    if [ ${file: -2} == ".c" ]
    then
-      echo -n -e "testing $file\t\t"
+      echo -n -e "$file\t\t\t\t\t\t"
       my_file=`echo "$file" | cut -d\. -f1` 
       a=`binChecker $my_file`
+      if [[ "$i" == "1" || "$i" == "3" || "$i" == "5" || "$i" == "9" ]]
+      then
+	 echo -en "\t\t"
+      fi
+      if [[ "$i" == "2" || "$i" == "7" ]]
+      then
+	 echo -en "\t\t\t" 
+      fi
+      if [[ "$i" == "4" || "$i" == "6" || "$i" == "10" || "$i" == "11" || "$i" == "12" ]]
+      then
+	 echo -en "\t"
+      fi
       if [[ "$a" -ne "1" ]]
       then
 	 echo -e "\033[0;32mSUCCESS\033[0m"
+	 let "j+=1"
       else
-	 echo -e "\033[0;31mFAILED\033[0m"
+	 echo -e "\033[0;31m FAILED\033[0m"
       fi
+      let "i+=1"
    fi
 done
+let "i-=1"
+echo -ne "\n\n\n\t\t\t\t\t\t\t Success rate: "
+echo -n $(( j * 100 / i))
+echo -e "%\n\n\n"
 #rm -f *.bc *.txt my_bc *.ll
 cd ..
