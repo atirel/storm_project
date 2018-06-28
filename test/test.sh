c5f8ccf3d534b9a729bf693166a86a4a27d18989
guest <25740490+atirel@users.noreply.github.com>
2018-06-28 17:31:25 +0200

    simplified display and functional cuts

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 place=`pwd`/src-test-files
+COLS=$(tput cols)
 cd $place
 function binGenerator {
    clang -emit-llvm -c -o $1.bc $1.c
@@ -9,7 +10,13 @@ function binGenerator {
 }
 
 function binChecker {
-  DIFF=$(diff $1.txt ../expected-results-tst/$1.txt)
+   if [[ ! -e ../../expected-results-tst/$1.txt ]]
+   then
+      echo 2
+#      ultimateCleaner `pwd`
+      return
+   fi
+  DIFF=$(diff $1.txt ../../expected-results-tst/$1.txt)
    if [[ $DIFF != "" ]]
    then
       echo 1
@@ -17,67 +24,88 @@ function binChecker {
    fi
    echo 0
 }
+
+SUC=`echo -e "\033[0;92mSUCCESS\033[0m"`
+FAIL=`echo -e "\033[0;91mFAILURE\033[0m"`
+
+function fatalTestor {
+   let "i=1"
+   for file in $(ls)
+   do
+      if [ ${file: -2} == ".c" ]
+      then
+	 my_file=`echo "$file" | cut -d\. -f1`
+	 binGenerator $my_file $i 
+      fi
+      let "i+=1"
+   done
+}
+
+function fatalDisplayer {
+   let "j=0"
+   let "i=1"
+   for file in $(ls)
+   do
+      if [ ${file: -2} == ".c" ]
+      then
+   	 echo -n -e "\n$file"
+   	 my_file=`echo "$file" | cut -d\. -f1` 
+   	 sizename=${#file}
+   	 a=`binChecker $my_file`
+   	 if [[ "$a" == "0" ]]
+   	 then
+   	    printf "%*s" $[$COLS/2-sizename] "$SUC"
+   	    let "j+=1"
+	 elif [[ "$a" == "2" ]]
+	 then
+	    printf "\ttest not found\n"
+	    exit 0
+	 else
+   	    printf "%*s" $[$COLS/2-sizename] "$FAIL"
+   	 fi
+   	 let "i+=1"
+      fi
+   done
+   let "i-=1"
+   echo -e "\n\n\n"
+   printf "%*s" $[$COLS/2-10] "Success rate: "
+   successRate=$(( j * 10000 / i))
+   if [[ "$successRate" -ge "7500" ]]
+   then
+      echo -en "\033[0;92m"
+   elif [[ "$successRate" -ge "5000" ]]
+   then
+      echo -en "\033[0;93m"
+   else
+      echo -ne "\033[0;91m"
+   fi
+   echo -e "$(( successRate / 100)).$(( successRate % 100))%\n\n\n\033[0m"
+   rm -f *.bc my_bc *.ll
+}
+
+function ultimateCleaner {
+   rm -f $1/*.ll $1/*.txt $1/*.bc $1/*.s
+}
+
 clear
 echo "Do not worry if you see useless stuff for testing purposes, the execution is written over cerrs and as far as my knowledge goes, i don't know a way to hide it while saving it"
 echo "for convention, everything will be erased before showing test results"
-echo -e "\n\n\n\t\t\t\t\t\t\tpress a key to continue"
+printf "\n\n\n\n\n%*s\n\n\n\n\n" $[$COLS/2] "press a key to continue"
 read continuation
-
-let "i=1"
-for file in $(ls)
-do
-   if [ ${file: -2} == ".c" ]
-   then
-      my_file=`echo "$file" | cut -d\. -f1`
-      binGenerator $my_file $i 
-   fi
-   let "i+=1"
-done
-
+let total=0
+cd basic_c_tests
+fatalTestor
+let "total+=i"
+cd ../c_array_tests
+fatalTestor
+let "total+=i"
 clear
-let "i=1"
-let "j=0"
-for file in $(ls)
-do
-   if [ ${file: -2} == ".c" ]
-   then
-      echo -n -e "$file\t\t\t\t\t\t"
-      my_file=`echo "$file" | cut -d\. -f1` 
-      a=`binChecker $my_file`
-      if [[ "$i" == "1" || "$i" == "3" || "$i" == "5" || "$i" == "9" ]]
-      then
-	 echo -en "\t\t"
-      fi
-      if [[ "$i" == "2" || "$i" == "7" ]]
-      then
-	 echo -en "\t\t\t" 
-      fi
-      if [[ "$i" == "4" || "$i" == "6" || "$i" == "10" || "$i" == "11" || "$i" == "12" ]]
-      then
-	 echo -en "\t"
-      fi
-      if [[ "$a" -ne "1" ]]
-      then
-	 echo -e "\033[0;32mSUCCESS\033[0m"
-	 let "j+=1"
-      else
-	 echo -e "\033[0;31m FAILED\033[0m"
-      fi
-      let "i+=1"
-   fi
-done
-let "i-=1"
-echo -ne "\n\n\n\t\t\t\t\t\t\t Success rate: "
-succesRate=$(( j * 10000 / i))
-if [[ "$succesRate" -ge "7500" ]]
-then
-   echo -en "\033[0;92m"
-elif [[ "$succesRate" -ge "5000" ]]
-then
-   echo -en "\033[0;93m"
-else
-   echo -ne "\033[0;91m"
-fi
-echo -e "$(( succesRate / 100)).$(( succesRate % 100))%\n\n\n\033[0m"
-rm -f *.bc my_bc *.ll *.txt
+printf "\n\n\t\t%*s\n\n" $[$COLS/2] "Testing basic C functions without condition or loop or array"
+cd ../basic_c_tests
+fatalDisplayer
+ultimateCleaner `pwd`
+cd ../c_array_tests
+printf "\n\n\t\t%*s\n\n" $[COLS/2] " Testing more complex C programs with cond and arrays"
+fatalDisplayer
+#ultimateCleaner `pwd`
 cd ..
