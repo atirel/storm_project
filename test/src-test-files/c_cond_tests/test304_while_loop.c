464f87f8e89fe69e8104646d1baa36174ad1837f
guest <25740490+atirel@users.noreply.github.com>
2018-07-17 16:48:42 +0200

    update test

--- test/src-test-files/c_cond_tests/test304_while_loop.c
+++ test/src-test-files/c_cond_tests/test304_while_loop.c
@@ -1,7 +1,12 @@
+#include <stdio.h>
+
 int main(){
-   int k = 90000000;
+   int k = 1048576;
+   int exec = 0;
    while(k){
       k /= 2;
+      exec++;
    }
+   printf("%d\n", exec);
    return 0;
 }
